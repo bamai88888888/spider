e186df051f746f54eb8072544f5dd98a1050ff98
八脉 <1254980598@qq.com>
2017-10-17 19:01:24 +0800

    aqy--电影排行榜--热播榜
    aqy--电影排行榜--热播榜

---
+++ aiqiyi_2.php
@@ -0,0 +1,27 @@
+<?php
+// 爱奇艺--电影排行榜--热播榜
+include_once 'simple_html_dom.php';
+$html = file_get_html('http://www.iqiyi.com/dianying_new/i_list_paihangbang.html');
+foreach(str_get_html($html->find('.wrapper-piclist',0)->innertext)->find('.J_videoLi') as $element){
+		echo $element->children(0)->children(0)->href; //url地址
+		echo $element->children(0)->children(0)->children(0)->src; //图片地址
+		echo $element->children(1)->children(0)->children(0)->plaintext; //评分
+		echo $element->children(1)->children(0)->children(1)->plaintext; //电影名字
+		echo "<br>";
+}
+// foreach(str_get_html($html->find('.no1',0)->innertext)->find('.no2') as $element){
+// 	print $element->children(0)->plaintext;
+// 	print $element->children(1)->plaintext;
+// 	echo '<br>';
+// }
+// foreach($html->find('.wrapper-piclist') as $element){
+// 	foreach($element->find('.J_videoLi') as $s){
+// 		echo $s->children(0)->children(0)->href; //url地址
+// 		echo $s->children(0)->children(0)->children(0)->src; //图片地址
+// 		echo $s->children(1)->children(0)->children(0)->plaintext; //评分
+// 		echo $s->children(1)->children(0)->children(1)->plaintext; //电影名字
+// 		echo "1<br>";
+
+// 	}
+// }
+?>
